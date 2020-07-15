4c95f05a04df953a5ba54c1fbf9200797de16485
Dave Forgac <tylerdave@tylerdave.com>
2020-07-15 01:46:38 -0400

    install allauth and blacken

--- discoreg/discoreg/asgi.py
+++ discoreg/discoreg/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discoreg.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "discoreg.settings")
 
 application = get_asgi_application()
--- discoreg/discoreg/urls.py
+++ discoreg/discoreg/urls.py
@@ -4,4 +4,4 @@ from django.urls import include, path
 urlpatterns = [
     path("registrations/", include("registrations.urls")),
     path("admin/", admin.site.urls),
-    ]
+]
