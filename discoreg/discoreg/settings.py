9d3b2768870a1c4290dbcb445eed77454fbef185
Dave Forgac <tylerdave@tylerdave.com>
2020-07-15 01:50:42 -0400

    configure allauth

--- discoreg/discoreg/settings.py
+++ discoreg/discoreg/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
     "django.contrib.staticfiles",
     "allauth",
     "allauth.account",
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+SITE_ID = 1
+
--- discoreg/discoreg/urls.py
+++ discoreg/discoreg/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("registrations/", include("registrations.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
