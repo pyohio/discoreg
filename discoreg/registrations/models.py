4af95140083f2fefa7a3372efecf0fbe94075686
Dave Forgac <tylerdave@tylerdave.com>
2020-07-19 02:35:07 -0400

    add initial models

--- discoreg/registrations/models.py
+++ discoreg/registrations/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class DiscordServer(models.Model):
+    server_id = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class DiscordRole(models.Model):
+    discord_role_id = models.CharField(max_length=32)
+    discord_server = models.ForeignKey(DiscordServer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class EmailRole(models.Model):
+    email = models.EmailField()
+    discord_roles = models.ManyToManyField(DiscordRole)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Registration(models.Model):
+    email = models.ForeignKey(EmailRole, on_delete=models.CASCADE)
+    reference_id = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
