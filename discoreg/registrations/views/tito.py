9f6cc41913c2926590d43c571a457b5e24f80580
Dave Forgac <tylerdave@tylerdave.com>
2020-07-19 02:20:59 -0400

    simple webhook handling

--- discoreg/registrations/views/tito.py
+++ discoreg/registrations/views/tito.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+import json
+import logging
+import os
 
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+logger = logging.getLogger(__name__)
+
+TITO_WEBHOOK_TOKEN = os.environ.get('TITO_WEBHOOK_TOKEN')
+
+@csrf_exempt
 def tito_webhook(request):
-    return HttpResponse("Registration!")
+    if request.META.get('HTTP_AUTHORIZATION') != f"Bearer {TITO_WEBHOOK_TOKEN}":
+        return HttpResponse("Unauthorized", status=401)
+
+    payload = json.loads(request.body.decode("utf-8"))
+    logger.warning(payload)
+    return HttpResponse(status=201)
