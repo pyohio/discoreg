41d18b0c62ea92d250a12d9108fff779be78b980
Dave Forgac <tylerdave@tylerdave.com>
2023-12-15 00:01:29 -0500

    remove debug message

--- discoreg/nextupbot/management/commands/nextupbot.py
+++ discoreg/nextupbot/management/commands/nextupbot.py
@@ -76,9 +76,6 @@ class BotClient(discord.Client):
     async def on_ready(self):
         print("Logged on as {0}!".format(self.user))
 
-    async def on_message(self, message):
-        print("Message from {0.author}: {0.content}".format(message))
-
     async def my_background_task(self):
         await self.wait_until_ready()
         # counter = 0
